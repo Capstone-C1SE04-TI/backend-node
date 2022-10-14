58f5a4571fab8d642036c89ffe88130165f9cd1c
huynhdthanhtuan <tuan40655@gmail.com>
2022-10-14 09:10:30 +0700

    Continue handle logic for this API

--- src/controllers/ForgotPassword.js
+++ src/controllers/ForgotPassword.js
@@ -1,82 +1,138 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
 const nodemailer = require("nodemailer");
-const { randomConfirmationCode } = require("../helpers");
+const { OAuth2Client } = require("google-auth-library");
+
 const {
 	getUserByEmail,
 	updateUserConfirmationCode,
 } = require("../services/crud-database/user");
 const { validateSubmitCodeBody } = require("../validators/user");
+const { randomConfirmationCode } = require("../helpers");
+
+const {
+	GOOGLE_MAILER_CLIENT_ID,
+	GOOGLE_MAILER_CLIENT_SECRET,
+	GOOGLE_MAILER_REFRESH_TOKEN,
+	ADMIN_EMAIL_ADDRESS,
+	ADMIN_EMAIL_PASSWORD,
+} = process.env;
+
+const myOAuth2Client = new OAuth2Client(
+	GOOGLE_MAILER_CLIENT_ID,
+	GOOGLE_MAILER_CLIENT_SECRET,
+);
+myOAuth2Client.setCredentials({
+	refresh_token: GOOGLE_MAILER_REFRESH_TOKEN,
+});
 
 function ForgotPasswordController() {
-	// ==> ERROR
+	// ==> DOING
 	this.submitEmail = async (req, res, next) => {
-		const user = await getUserByEmail(req.body.email);
+		// Get OAuth token
+		// const getOAuth = async function () {
+		// 	return await fetch("https://api.petfinder.com/v2/oauth2/token", {
+		// 		method: "POST",
+		// 		body:
+		// 			"grant_type=client_credentials&client_id=" +
+		// 			GOOGLE_MAILER_CLIENT_ID +
+		// 			"&client_secret=" +
+		// 			GOOGLE_MAILER_CLIENT_SECRET,
+		// 		headers: {
+		// 			"Content-Type": "application/x-www-form-urlencoded",
+		// 		},
+		// 	})
+		// 		.then(function (res) {
+		// 			return res.json();
+		// 		})
+		// 		.then(function (data) {
+		// 			console.log(data);
+		// 			// Store token data
+		// 			token = data.access_token;
+		// 			tokenType = data.token_type;
 
-		const transporter = nodemailer.createTransport({
-			service: "gmail",
-			host: "smtp.gmail.com",
-			port: 465,
-			secure: true,
-			auth: {
-				user: process.env._0x072126sajkxja3181sc33242315_,
-				pass: process.env._0x073126sajkxja3112lkkjs83j22_,
-			},
-			tls: {
-				rejectUnauthorized: false,
-			},
-		});
+		// 			console.log(token);
+		// 			console.log(tokenType);
+		// 			console.log(expires);
 
-		const code = randomConfirmationCode();
-		const html = `
-            <div style="padding: 10px; background-color: #003375">
-                <div style="padding: 10px; background-color: white;">
-                    <h4 style="color: #0085ff">Your verification code is:</h4>
-                    <span style="color: black">${code}</span>
-                </div>
-            </div>
-        `;
+		// 			expires = new Date().getTime() + data.expires_in * 1000;
+		// 			console.log(expires);
+		// 		});
+		// };
 
-		const mailOptions = {
-			from: {
-				name: "Tracking Investor's Support Team",
-				address: process.env._0x072126sajkxja3181sc33242315_,
-			},
-			to: req.body.email,
-			subject: "Verify code to create new password - TI Team",
-			html: html,
-		};
+		// await getOAuth();
 
-		transporter.sendMail(mailOptions, async (error, info) => {
-			if (!error) {
-				if (user) {
-					const updatedUser = await updateUserConfirmationCode(
-						user.docId,
-						code,
-					);
+		// // Make call if token expired
+		// const makeCall = () => {
+		// 	// If current token is invalid, get a new one
+		// 	if (!expires || expires - new Date().getTime() < 1) {
+		// 		getOAuth().then(function () {
+		// 			// use access token
+		// 		});
+		// 	}
+		// };
 
-					if (updatedUser) {
-						return res.status(200).json({
-							message:
-								"Submit email successfully. Please check your email",
-						});
-					} else {
-						return res.json({
-							message: "Submit email failed",
-						});
-					}
-				} else {
-					return res.json({ message: "Email not found" });
-				}
-			} else {
-				return res
-					.status(400)
-					.json({ message: "Submit email failedd" });
-			}
-		});
+		//const user = await getUserByEmail(req.body.email);
+
+		// if (user) {
+		try {
+			// Send mail
+			const myAccessTokenObject = await myOAuth2Client.getAccessToken();
+			const myAccessToken = myAccessTokenObject?.token;
+
+			const transport = nodemailer.createTransport({
+				service: "gmail",
+				auth: {
+					type: "OAuth2",
+					user: ADMIN_EMAIL_ADDRESS,
+					pass: ADMIN_EMAIL_PASSWORD,
+					clientId: GOOGLE_MAILER_CLIENT_ID,
+					clientSecret: GOOGLE_MAILER_CLIENT_SECRET,
+					refreshToken: GOOGLE_MAILER_REFRESH_TOKEN,
+					accessToken: myAccessToken,
+				},
+			});
+
+			const mailOptions = {
+				from: {
+					name: "Tracking Investment's Support Team",
+					address: ADMIN_EMAIL_ADDRESS,
+				},
+				to: req.body.email,
+				subject: "Reset Password - Tracking Investment",
+				html: `
+						<div>
+							<div>
+								<h4 style=font-size: 16px">Hi, I'm Hoang Dung from Tracking Investment's Support Team</h4>
+								</br>
+								<h4 style=font-size: 16px">Your reset password code is:</h4>
+								<span style="color: black; font-size: 26px">${randomConfirmationCode()}</span>
+							</div>
+						</div>
+					`,
+			};
+
+			await transport.sendMail(mailOptions);
+
+			// Store confirmationCode
+			// await updateUserConfirmationCode(user.docId, code);
+
+			return res.status(200).json({
+				message: "successfully",
+			});
+		} catch (error) {
+			console.log(error);
+			return res.status(400).json({
+				message: "failed",
+			});
+		}
+		// } else {
+		// 	return res.status(400).json({ message: "email_notfound" });
+		// }
 	};
 
-	// ==> DONE
+	// ==> TODO
 	this.submitCode = async (req, res, next) => {
-		// Validate request body
 		const { status, error } = await validateSubmitCodeBody(req, res, next);
 
 		if (status === "failed") {
@@ -86,72 +142,15 @@ function ForgotPasswordController() {
 			const user = await getUserByEmail(email);
 
 			if (code === user.confirmationCode) {
-				return res.status(200).json({ message: "Correct code" });
+				return res.status(200).json({ message: "successfully" });
 			} else {
-				return res.status(400).json({ message: "Wrong code" });
+				return res.status(400).json({ message: "failed" });
 			}
 		}
 	};
 
 	// ==> TODO
-	this.resendCode = (req, res, next) => {
-		// const transporter = nodemailer.createTransport({
-		//     host: "smtp.gmail.com",
-		//     port: 465,
-		//     secure: true,
-		//     auth: {
-		//         user: process.env._0x072126sajkxja3181sc33242315_,
-		//         pass: process.env._0x073126sajkxja3112lkkjs83j22_,
-		//     },
-		//     tls: {
-		//         rejectUnauthorized: false,
-		//     },
-		// });
-		// const code = randomConfirmationCode();
-		// const html = `
-		//     <div style="padding: 10px; background-color: #003375">
-		//         <div style="padding: 10px; background-color: white;">
-		//             <h4 style="color: #0085ff">Your verification code is:</h4>
-		//             <span style="color: black">${code}</span>
-		//         </div>
-		//     </div>
-		// `;
-		// const mainOptions = {
-		//     from: {
-		//         name: "LOP's Support Team",
-		//         address: "baop38391@gmail.com",
-		//     },
-		//     to: req.body.email,
-		//     subject: "Verify code to create new password - LOP",
-		//     html: html,
-		// };
-		// transporter.sendMail(mainOptions, (err, info) => {
-		//     if (!err) {
-		//         // Lưu code vào DB
-		//         User.findOne({ email: req.body.email }, (error, user) => {
-		//             if (user) {
-		//                 console.log(user);
-		//                 user.updateOne({ code: code }, (error, updatedUser) => {
-		//                     console.log(user);
-		//                     if (updatedUser) {
-		//                         return res.json({
-		//                             message:
-		//                                 "Re-send code success. Please check your email",
-		//                         });
-		//                     } else {
-		//                         return res.json({ message: "Re-send code failed" });
-		//                     }
-		//                 });
-		//             } else {
-		//                 return res.json({ message: "Re-send code failed" });
-		//             }
-		//         });
-		//     } else {
-		//         return res.json({ message: "Re-send code failed" });
-		//     }
-		// });
-		return res.status(200).json({ message: "Successfully" });
-	};
+	this.resendCode = (req, res, next) => {};
 
 	// ==> TODO
 	this.createNewPassword = (req, res, next) => {
@@ -175,7 +174,7 @@ function ForgotPasswordController() {
 		//         return res.json({ message: "Create new password failed" });
 		//     }
 		// });
-		return res.status(200).json({ message: "Successfully" });
+		// return res.status(200).json({ message: "Successfully" });
 	};
 }
 
